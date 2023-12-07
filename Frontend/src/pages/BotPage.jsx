bd69b6089e2d7b4868ecb12bb0158016c5697fa9
Altan <suyanovaltan@gmail.com>
2023-12-07 16:00:50 +0300

    BotPage first commit

--- Frontend/src/pages/BotPage.jsx
+++ Frontend/src/pages/BotPage.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react';
 
 export function Bot() {
+
+    const [companyShares, setCompanyShares] = useState([
+        { name: 'sber' },
+        { name: 'tinkoff' }
+    ]);
+
     return (
         <div className='botPage'>
-            Это страница с ботом
+            <div className='investMenu'>
+                <ul className='companySharesList'>
+                    {companyShares.map(share => <li>{share.name}</li>)}
+                </ul>
+            </div>
+            <div className='botSettings'>
+                <div className="settingsInputDiv">
+                    <p>Капитал</p>
+                    <input className='settingsInput'></input>
+                </div>
+                <div className="settingsInputDiv">
+                    <p>Процент допустимых потерь</p>
+                    <input className='settingsInput'></input>
+                </div>
+
+                <div className="settingsInputDiv">
+                    <p>Срок торговли</p>
+                    <input className='settingsInput'></input>
+                </div>
+
+                <button className='startButton'>Начать торги</button>
+            </div>
         </div>
     )
 }
