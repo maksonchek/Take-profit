faac3a0c9e235419e547822ba815a4858be00f8f
Artem Ishmukhamedov <fesevu@gmail.com>
2023-12-09 14:13:18 +0300

    Add files via upload
    закончил модуль получения онлайн данных и обработка данных

--- getDataOnline.py
+++ getDataOnline.py
@@ -33,7 +33,7 @@ def getCandles(start, end, tickers, period='D') -> pd.DataFrame:
    return df    
 
 def getOnlineDataOrderStats(tickers=None, filename = None):
-        onlineData = pd.DataFrame()
+        orderstats = pd.DataFrame()
         #k = 0
         #for date in dates:
         if tickers is not None:
@@ -88,64 +88,171 @@ def getOnlineDataTradeStats(tickers=None, filename = None) -> pd.DataFrame:
                 tradestats.to_csv(filename, index=None, sep=";")
         print('данные о статистике торгов за последние 5 минут получены')
         
-        return tradestats     
+        return tradestats       
 
-def start(tickers):
+def start(tickers, day_stop):
+   titles_tradeStats = ['tradedate',
+                'tradetime',
+                'secid',
+                'pr_open',
+                'pr_high',
+                'pr_low',
+                'pr_close',
+                'pr_std',
+                'vol',
+                'val',
+                'trades',
+                'pr_vwap',
+                'pr_change',
+                'trades_b',
+                'trades_s',
+                'val_b',
+                'val_s',
+                'vol_b',
+                'vol_s',
+                'disb',
+                'pr_vwap_b',
+                'pr_vwap_s',
+                'SYSTIME']
    
-   now_day = datetime.date.today() #сегодня
+   titles_orderStats = ['tradedate',
+                'tradetime',
+                'secid',
+                'put_orders_b',
+                'put_orders_s',
+                'put_val_b',
+                'put_val_s',
+                'put_vol_b',
+                'put_vol_s',
+                'put_vwap_b',
+                'put_vwap_s',
+                'put_vol',
+                'put_val',
+                'put_orders',
+                'cancel_orders_b',
+                'cancel_orders_s',
+                'cancel_val_b',
+                'cancel_val_s',
+                'cancel_vol_b',
+                'cancel_vol_s',
+                'cancel_vwap_b',
+                'cancel_vwap_s',
+                'cancel_vol',
+                'cancel_val',
+                'cancel_orders',
+                'SYSTIME']
+
+   # now_day = datetime.date.today() #сегодня   
+   now_day = datetime.date(2023, 12, 3) 
+   while True: #now_day < day_stop:
    
-   #now_day = datetime.date(2023, 12, 8)
+        now_day = datetime.date.today() #сегодня
+        save_date = None
+
+        now_time = datetime.datetime.now().time()
+        now_time = datetime.time(now_time.hour, now_time.minute, now_time.second)
+        save_time = None
+
+        if now_day != save_date or save_date is None:
    
-   yesterday = now_day - datetime.timedelta(days=1) #вчера
-   
-   week = yesterday - datetime.timedelta(days=7) #неделя от вчерашнего дня
-   
-   week_candle:pd.DataFrame = getCandles(yesterday, week, tickers, 'D') #свечи по интересущим нас тикерам за неделю
-   
-   #получаем и агрегируем данные за сегодняшний день о статистике торгов
-   tradeStats_today:pd.DataFrame = getDataTradeStats.getDataTicker(None, now_day, now_day)
-   hourTradeStats_today:list = getDataTradeStats.getDataHourTradeStats(tradeStats_today, True, tickers)
-   
-   #получаем и агрегируем данные за сегодняшний день о статистике заявок
-   orderStats_today:pd.DataFrame = getDataOrderStats.getDataTicker(None, now_day, now_day)
-   hourOrderStats_today:list = getDataOrderStats.getDataHourOrderStats(orderStats_today, True, tickers)
-   
-   #получаем агрегированные данные за последний час о статистике торгов
-   lastHourTradeStats:list = hourTradeStats_today[-len(tickers):]
-   print(lastHourTradeStats)
-   
-   #получаем агрегированные данные за последний час о статистике заявок
-   lastHourOrderStats:list = hourOrderStats_today[-len(tickers):]
-   print(lastHourOrderStats)
-   
-   
-   #если алгоритм запущен ночью и получены данные не за сегодня
-   if datetime.datatime.strptime(lastHourTradeStats[0][0], '%Y %m %d') != now_day:
-         lastHourTradeStats = []
-         lastHourOrderStats = []
-         
-   while True:
-      nowTradeStats = getOnlineDataTradeStats(tickers).values.tolist()   
-      nowOrderStats = getOnlineDataOrderStats(tickers).values.tolist()  
-      
-      if len(lastHourTradeStats) == 0:
-         lastHourTradeStats = nowTradeStats
-         lastHourOrderStats = nowOrderStats
-         
-         #здесб продолжить, нужно добавить корректное время и обрабатывать
-      else: #если данные какие то уже есть за последний час
-            
-         
-      
-      
-         
-            
-    
+                #now_day = datetime.date(2023, 12, 8)
+                now_day = datetime.date.today() #сегодня
+        
+                yesterday = now_day - datetime.timedelta(days=1) #вчера
+        
+                week = yesterday - datetime.timedelta(days=7) #неделя от вчерашнего дня
+        
+                week_candle:pd.DataFrame = getCandles(yesterday, week, tickers, 'D') #свечи по интересущим нас тикерам за неделю
+
+                print("Отдали Максу свечи")
+                #отдаем Максу return week_candle
+        
+                #получаем и агрегируем данные за сегодняшний день о статистике торгов
+                tradeStats_today:pd.DataFrame = getDataTradeStats.getDataTicker(None, now_day, now_day)
+                hourTradeStats_today:list = getDataTradeStats.getDataHourTradeStats(tradeStats_today, True, tickers)
+        
+                #получаем и агрегируем данные за сегодняшний день о статистике заявок
+                orderStats_today:pd.DataFrame = getDataOrderStats.getDataTicker(None, now_day, now_day)
+                hourOrderStats_today:list = getDataOrderStats.getDataHourOrderStats(orderStats_today, True, tickers)
+        
+                #получаем агрегированные данные за последний час о статистике торгов
+                lastHourTradeStats:list = hourTradeStats_today[-len(tickers):]
+        
+                #получаем агрегированные данные за последний час о статистике заявок
+                lastHourOrderStats:list = hourOrderStats_today[-len(tickers):]
+
+                save_date = now_day
+        
+        #если алгоритм запущен ночью и получены данные не за сегодня
+        # if datetime.datatime.strptime(lastHourTradeStats[0][0], '%Y %m %d') != now_day:
+        #         lastHourTradeStats = []
+        #         lastHourOrderStats = []
+
+        #получаем онлайн данные
+        nowTradeStats = getOnlineDataTradeStats(tickers).values.tolist()   
+        nowOrderStats = getOnlineDataOrderStats(tickers).values.tolist() 
+
+        last_data_time = datetime.datetime.strptime(nowTradeStats[0][1], "%H:%M:%S").time()
+        if last_data_time != save_time or save_time is None:
+                #Проверка на новый час
+                prev_time = lastHourOrderStats[0][0] + " " + lastHourOrderStats[0][1]
+                prev_time = datetime.datetime.strptime(prev_time, "%Y-%m-%d %H:%M:%S") + datetime.timedelta(minutes=5)
+                cur_time = nowOrderStats[0][0] + " " + nowOrderStats[0][1]
+                cur_time = datetime.datetime.strptime(cur_time, "%Y-%m-%d %H:%M:%S")
+                time_diff = cur_time - prev_time
+                #начался новый час
+                if time_diff.total_seconds() >= 3600:
+                        if lastHourTradeStats[0] == titles_tradeStats:
+                                lastHourTradeStats.pop(0)
+                        if lastHourOrderStats[0] == titles_orderStats:
+                                lastHourOrderStats.insert(titles_orderStats)
+
+                        lastHourTradeStats = pd.DataFrame((lastHourTradeStats + nowTradeStats), columns=titles_tradeStats)
+                        lastHourOrderStats = pd.DataFrame((lastHourOrderStats + nowOrderStats), columns=titles_orderStats) 
+
+                        print("Отдали Максу статистику")        
+                        #отдать Максу return lastHourTradeStats, lastHourOrderStats               
+
+                        lastHourTradeStats = nowTradeStats
+                        lastHourOrderStats = nowOrderStats
+                        for x in lastHourTradeStats:         
+                                time_beg = datetime.datetime.strptime(x[1], "%H:%M:%S")
+                                hours = time_beg.replace(minute=0, second=0, microsecond=0)
+                                x[1] = str(hours.strftime("%H:%M:%S"))
+
+                        for x in lastHourOrderStats:         
+                                time_beg = datetime.datetime.strptime(x[1], "%H:%M:%S")
+                                hours = time_beg.replace(minute=0, second=0, microsecond=0)
+                                x[1] = str(hours.strftime("%H:%M:%S"))
+                else:   
+                        if lastHourTradeStats[0] == titles_tradeStats:
+                                lastHourTradeStats.pop(0)
+                        if lastHourOrderStats[0] == titles_orderStats:
+                                lastHourOrderStats.pop(0) 
+
+                        lastHourTradeStats = pd.DataFrame((lastHourTradeStats + nowTradeStats), columns=titles_tradeStats)
+                        lastHourOrderStats = pd.DataFrame((lastHourOrderStats + nowOrderStats), columns=titles_orderStats) 
+
+                        lastHourTradeStats:list = getDataTradeStats.getDataHourTradeStats(lastHourTradeStats, True, tickers)
+                        lastHourOrderStats:list = getDataOrderStats.getDataHourOrderStats(lastHourOrderStats, True, tickers)  
+
+                # if len(lastHourTradeStats) == 0:
+                #         lastHourTradeStats = nowTradeStats
+                #         lastHourOrderStats = nowOrderStats
+                #         #здесб продолжить, нужно добавить корректное время и обрабатывать
+                # else: #если данные какие то уже есть за последний час
+                #         pass
+        else:
+                pass
+
+        time.sleep(150)
+
+        #now_day = now_day + datetime.timedelta(days=1)
 
 if __name__ == '__main__':
    tickers = ['SBER', 'GAZP', 'ROSN', 'NVTK', 'GMKN', 'YNDX', 'LKOH', 'PLZL', 'MOEX', 'PHOR']
    
-   start(tickers)
+   start(tickers, datetime.date(2023, 12, 7))
 
 
 
